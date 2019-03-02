323d6f40d02da89f4ac45407e4172df0494f59a9
burgerhex <aviv.shai@gmail.com>
2019-03-02 15:09:35 -0800

    Fix another error in isPrime

--- src/Helpers.java
+++ src/Helpers.java
@@ -17,7 +17,7 @@ public final class Helpers {
         if (num < 2)
             return false;
         else {
-            for (int i = 2; i < Math.sqrt(num); i++) {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
                 if (num % i == 0)
                     return false;
             }
