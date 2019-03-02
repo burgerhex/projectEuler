174a71d09bfd81db9d995964d71b9907fabc5bed
burgerhex <aviv.shai@gmail.com>
2019-03-02 12:08:39 -0800

    Add problem framework with solve method that instances must override

--- src/Problem.java
+++ src/Problem.java
@@ -1,5 +1,9 @@
 public abstract class Problem {
 
+    /**
+     * Solves this problem from ProjectEuler.
+     * @return the integer solution to this problem
+     */
     public abstract int solve();
 
 }
