b12513bcac0bc1ac1bd904ddccd9c3689d7ba047
burgerhex <aviv.shai@gmail.com>
2019-03-02 15:21:31 -0800

    Add problem 8 and change return values of solve

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
      * @see Main#printProblem(int) printProblem
      */
     public static void main(String[] args) {
-        printProblems(1, 7);
+        printProblems(1, 8);
     }
 
     /**
@@ -24,7 +24,7 @@ public class Main {
      */
     private static final Problem PROBLEM1 = new Problem() {
         @Override
-        public int solve() {
+        public long solve() {
             int sum = 0;
 
             for (int i = 1; i < 1000; i++) {
@@ -42,7 +42,7 @@ public class Main {
      */
     private static final Problem PROBLEM2 = new Problem() {
         @Override
-        public int solve() {
+        public long solve() {
             int sum = 0;
 
             for (int n = 0; Helpers.fib(n) <= 4000000; n++) {
@@ -59,7 +59,7 @@ public class Main {
      */
     private static final Problem PROBLEM3 = new Problem() {
         @Override
-        public int solve() {
+        public long solve() {
             int max = 0;
 
             for (int factor : Helpers.getPrimeFactors(600851475143L)) {
@@ -76,7 +76,7 @@ public class Main {
      */
     private static final Problem PROBLEM4 = new Problem() {
         @Override
-        public int solve() {
+        public long solve() {
             int max = 0;
 
             for (int i = 999; i >= 100; i--) {
@@ -94,7 +94,7 @@ public class Main {
      */
     private static final Problem PROBLEM5 = new Problem() {
         @Override
-        public int solve() {
+        public long solve() {
             int lcm = Helpers.lcm(1, 2);
 
             for (int check = 3; check <= 20; check++)
@@ -109,7 +109,7 @@ public class Main {
      */
     private static final Problem PROBLEM6 = new Problem() {
         @Override
-        public int solve() {
+        public long solve() {
             int sumOfSquares = 0;
             int sum          = 0;
 
@@ -129,7 +129,7 @@ public class Main {
      */
     private static final Problem PROBLEM7 = new Problem() {
         @Override
-        public int solve() {
+        public long solve() {
             ArrayList<Integer> primes = new ArrayList<>();
 
             primes.add(2);
@@ -139,6 +139,52 @@ public class Main {
             }
 
             return primes.get(10000);
+        }
+    };
+
+    /**
+     * Problem number 8 found at https://projecteuler.net/problem=8.
+     */
+    private static final Problem PROBLEM8 = new Problem() {
+        @Override
+        public long solve() {
+
+            String number =
+                    "73167176531330624919225119674426574742355349194934" +
+                    "96983520312774506326239578318016984801869478851843" +
+                    "85861560789112949495459501737958331952853208805511" +
+                    "12540698747158523863050715693290963295227443043557" +
+                    "66896648950445244523161731856403098711121722383113" +
+                    "62229893423380308135336276614282806444486645238749" +
+                    "30358907296290491560440772390713810515859307960866" +
+                    "70172427121883998797908792274921901699720888093776" +
+                    "65727333001053367881220235421809751254540594752243" +
+                    "52584907711670556013604839586446706324415722155397" +
+                    "53697817977846174064955149290862569321978468622482" +
+                    "83972241375657056057490261407972968652414535100474" +
+                    "82166370484403199890008895243450658541227588666881" +
+                    "16427171479924442928230863465674813919123162824586" +
+                    "17866458359124566529476545682848912883142607690042" +
+                    "24219022671055626321111109370544217506941658960408" +
+                    "07198403850962455444362981230987879927244284909188" +
+                    "84580156166097919133875499200524063689912560717606" +
+                    "05886116467109405077541002256983155200055935729725" +
+                    "71636269561882670428252483600823257530420752963450";
+            long maxProduct = 0;
+
+            for (int i = 0; i < number.length() - 12; i++) {
+                String sub = number.substring(i, i + 13);
+                long prod13 = 1;
+
+                for (char digit : sub.toCharArray()) {
+                    prod13 *= Integer.valueOf(String.valueOf(digit));
+                }
+
+                if (prod13 > maxProduct)
+                    maxProduct = prod13;
+            }
+
+            return maxProduct;
         }
     };
 
@@ -179,12 +225,12 @@ public class Main {
         }
 
         long startTime = System.nanoTime();
-        int solution = problem.solve();
+        long solution = problem.solve();
         long endTime   = System.nanoTime();
 
         double seconds = (endTime - startTime) / 1000000000.0;
 
-        System.out.format("Solution to problem %3d: %10d [%.3f seconds]\n", num, solution, seconds);
+        System.out.format("Solution to problem %3d: %12d [%.3f seconds]\n", num, solution, seconds);
     }
 
 }
