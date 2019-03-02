6bef7a67d2cb3d0143992636b3770916a33ef186
burgerhex <aviv.shai@gmail.com>
2019-03-02 11:16:16 -0800

    Create file with problem 1

--- src/Tester.java
+++ src/Tester.java
@@ -1,3 +1,25 @@
 public class Tester {
 
+    public static void main(String[] args) {
+        print(problem1());
+    }
+
+    private static <T> void print(T answer) {
+        System.out.println(answer);
+    }
+
+    private static int problem1() {
+        // https://projecteuler.net/problem=1
+
+        int sum = 0;
+
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
 }
